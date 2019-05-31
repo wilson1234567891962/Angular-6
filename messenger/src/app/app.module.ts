baa5c45225a6595d5d2d99b46f365e33d7f4a6de
Sem - 6 - INGENIERIA INDUSTRIAL: Wilson Alejandro Gonzalez Gaitan <wilsongonzalez.ag@academia.umb.edu.co>
2019-05-30 22:13:26 -0500

    Implements the module about routing link

--- messenger/src/app/app.module.ts
+++ messenger/src/app/app.module.ts
@@ -1,10 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { ConversationComponent } from './conversation/conversation.component';
-import { ProfileComponent } from './profile/profile.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import {ConversationComponent} from './conversation/conversation.component';
+import {ProfileComponent} from './profile/profile.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRouters: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'conversation', component: ConversationComponent}
+];
 
 @NgModule({
   declarations: [
@@ -15,9 +24,12 @@ import { ProfileComponent } from './profile/profile.component';
     ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRouters)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
